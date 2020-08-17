44cf21d0eae4714cf04486fec2af903cd4e00be7
jiaxuan-dev <15702979536@163.com>
2020-08-17 15:58:12 +0800

    f动态网格修改

--- src/components/mesh-panel/meshPanel.vue
+++ src/components/mesh-panel/meshPanel.vue
@@ -9,12 +9,8 @@
     @close="setVisible"
   >
     <div class="input">
-      <!-- <div class="con">
-        <input multiple style="width: 70px;display:none" ref="file" @input="read" type="file" />
-        <el-button @click="setdata" plain>导入数据</el-button>
-      </div>-->
       <div class="con">
-        <el-select v-model="colorMapValue" placeholder="请先选择色带再点击开始">
+        <el-select v-model="colorMapValue" placeholder="点击选择色带">
           <el-option
             v-for="item in colorOptions"
             :key="item.value"
@@ -30,6 +26,10 @@
       </div>
       <div class="con">
         <el-slider @input="changeSpeed" v-model="intervalTime"></el-slider>
+      </div>
+      <div class="con">
+        <input multiple style="width: 70px;display:none" ref="file" @input="read" type="file" />
+        <el-button @click="setdata" plain>导入本地新数据</el-button>
       </div>
     </div>
   </dialogPage>
@@ -123,68 +123,74 @@ export default {
       this.filePath = []
       this.PauseOrContinuetext = '暂停'
       this.PauseOrContinueflag = false
+      this.mesh = undefined
     },
     begin() {
-      // var blob = new Blob([JSON.stringify(this.data), JSON.stringify(this.datasDes)], { type: '' })
-      // saveAs(blob, 'cjl.json')
-      this.mesh = new DynamicMesh(Viewer, { polygonPositons: meshData.data, des: meshData.datasDes, colorMap: this.colorMapValue })
+      if (this.mesh === undefined) {
+        this.mesh = new DynamicMesh(Viewer, {
+          polygonPositons: this.data.length === 0 ? JSON.parse(JSON.stringify(meshData.data)) : this.data,
+          des: this.datasDes.length === 0 ? JSON.parse(JSON.stringify(meshData.datasDes)) : this.datasDes,
+          colorMap: this.colorMapValue === '' ? 'jet' : this.colorMapValue
+        })
+      }
+
       this.$refs.file.value = ''
+    },
+    read(event) {
+      this.readFile(event.target.files)
+    },
+    readFile(file) {
+      let fileIndex = 0
+      let that = this
+      let reader = new FileReader()
+      reader.readAsText(file[fileIndex])
+      reader.onload = function() {
+        // console.log(this.result.split('\n'))
+        let allArray = this.result.split('\n')
+        let heightArray = []
+        for (const i in allArray) {
+          if (i > 5 && i < allArray.length - 1) {
+            let altmixarr = allArray[i].split(' ')
+            altmixarr.pop()
+            heightArray.push(altmixarr)
+          }
+        }
+        // console.log(heightArray)
+
+        let mixncols = allArray[0].split(' ')
+        let mixnrows = allArray[1].split(' ')
+        let mixlng = allArray[2].split(' ')
+        let mixlat = allArray[3].split(' ')
+        let mixcellsize = allArray[4].split(' ')
+        let dataDes = {}
+        dataDes.ncols = Number(mixncols[mixncols.length - 1])
+        dataDes.nrows = Number(mixnrows[mixnrows.length - 1])
+        dataDes.xllcorner = Number(mixlng[mixlng.length - 1])
+        dataDes.yllcorner = Number(mixlat[mixlat.length - 1])
+        dataDes.cellsize = Number(mixcellsize[mixcellsize.length - 1])
+
+        // console.log(dataDes)
+        // debugger
+        const positions = []
+        for (let j = 0; j < dataDes.nrows; j++) {
+          for (let i = 0; i < dataDes.ncols; i++) {
+            positions.push(j * dataDes.cellsize + dataDes.xllcorner)
+            positions.push(i * dataDes.cellsize + dataDes.yllcorner)
+            positions.push(Number(heightArray[j][i]))
+          }
+        }
+        that.datasDes.push(dataDes)
+        that.data.push(positions)
+        // console.log(that.data)
+        if (fileIndex < file.length - 1) {
+          fileIndex = fileIndex + 1
+          reader.readAsText(file[fileIndex])
+          // that.readFile(file)
+        } else {
+          fileIndex = 0
+        }
+      }
     }
-    // read(event) {
-    //   this.readFile(event.target.files)
-    // },
-    // readFile(file) {
-    //   let fileIndex = 0
-    //   let that = this
-    //   let reader = new FileReader()
-    //   reader.readAsText(file[fileIndex])
-    //   reader.onload = function() {
-    //     // console.log(this.result.split('\n'))
-    //     let allArray = this.result.split('\n')
-    //     let heightArray = []
-    //     for (const i in allArray) {
-    //       if (i > 5 && i < allArray.length - 1) {
-    //         let altmixarr = allArray[i].split(' ')
-    //         altmixarr.pop()
-    //         heightArray.push(altmixarr)
-    //       }
-    //     }
-    //     // console.log(heightArray)
-
-    //     let mixncols = allArray[0].split(' ')
-    //     let mixnrows = allArray[1].split(' ')
-    //     let mixlng = allArray[2].split(' ')
-    //     let mixlat = allArray[3].split(' ')
-    //     let mixcellsize = allArray[4].split(' ')
-    //     let dataDes = {}
-    //     dataDes.ncols = Number(mixncols[mixncols.length - 1])
-    //     dataDes.nrows = Number(mixnrows[mixnrows.length - 1])
-    //     dataDes.xllcorner = Number(mixlng[mixlng.length - 1])
-    //     dataDes.yllcorner = Number(mixlat[mixlat.length - 1])
-    //     dataDes.cellsize = Number(mixcellsize[mixcellsize.length - 1])
-
-    //     // console.log(dataDes)
-    //     // debugger
-    //     const positions = []
-    //     for (let j = 0; j < dataDes.nrows; j++) {
-    //       for (let i = 0; i < dataDes.ncols; i++) {
-    //         positions.push(j * dataDes.cellsize + dataDes.xllcorner)
-    //         positions.push(i * dataDes.cellsize + dataDes.yllcorner)
-    //         positions.push(Number(heightArray[j][i]))
-    //       }
-    //     }
-    //     that.datasDes.push(dataDes)
-    //     that.data.push(positions)
-    //     // console.log(that.data)
-    //     if (fileIndex < file.length - 1) {
-    //       fileIndex = fileIndex + 1
-    //       reader.readAsText(file[fileIndex])
-    //       // that.readFile(file)
-    //     } else {
-    //       fileIndex = 0
-    //     }
-    //   }
-    // }
   }
 }
 </script>
@@ -193,7 +199,7 @@ export default {
   padding: 25px;
 }
 .con {
-  margin: 0 0 10px 0;
+  margin: 0 0 5px 0;
 }
 </style>
 <style lang="scss">
